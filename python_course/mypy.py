3cbf5d64625005a7ff4ae14878e758463a95bdbd
Artiom <my@example.com>
2022-06-16 01:28:54 +0300

    first commit

---
+++ python_course/mypy.py
@@ -0,0 +1,13 @@
+# from urllib.request import HTTPDefaultErrorHandler
+
+
+# # a, b, c = map(int, input().split())
+# # lst = [a, b, c]
+# # # lst.sort()
+# # # print(lst[2] - lst[0])
+# # print(lst.sort())
+
+# a = "ahelllloou"
+# b= "hlello"
+# print(set(a))
+# print(set(b))
